14a116fb81ab28dbb5db44ac0e0a3ee73840a035
Ihor Khrystiuk <inanutshell86@gmail.com>
2018-07-23 13:03:29 +0300

    Add the Profile edit functionality.

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,9 @@ Route::prefix('admin')->group(function() {
 Route::prefix('user')->group(function() {
     Route::get('dashboard', 'UserController@dashboard')->name('userDashboard');
     Route::get('comments', 'UserController@comments')->name('userComments');
+    Route::get('profile', 'UserController@profile')->name('userProfile');
+    Route::post('profile', 'UserController@profilePost')->name('userProfilePost');
+
 });
 
 Route::prefix('author')->group(function() {
